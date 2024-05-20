a4de2d0fedc2586fc2550e2a3c4d932ad7485362
rennatiq <rachidennatiqi2000@gmail.com>
2024-05-20 10:40:19 -0400

    asd

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
-# WP_PATH="/var/www"
-
-# # Create or update wp-cli.yml
-# cat <<EOT > "$WP_PATH/wp-cli.yml"
-# path: $WP_PATH
-# database:
-#   dbname: $WORDPRESS_DB_NAME
-#   user: $WORDPRESS_DB_USER
-#   password: $WORDPRESS_DB_PASSWORD
-#   host: $WORDPRESS_DB_HOST
-# EOT
 
 if [ ! -f "wordpress/wp-config.php" ]; then
-	# wp core download --allow-root
-	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --path=wordpress --allow-root
-	wp core install --url="localhost" --title="Inception" --admin_user="$MARIADB_ADMIN_USER" \
-	--admin_password="$MARIADB_ADMIN_PWD" --admin_email="admin@gmail.com" --skip-email --path=wordpress --allow-root
-	wp user create $MARIADB_USER ana@gmail.com --role=author --user_pass=$MARIADB_PWD --path=wordpress --allow-root
+	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --path=wordpress  --allow-root
+	wp core install --url="$DOMAIN_NAME" --title="Inception" --admin_user="$MARIADB_ADMIN_USER" \
+	--admin_password="$MARIADB_ADMIN_PWD" --admin_email="$MARIADB_ADMIN_email" --skip-email --path=wordpress --allow-root
+	wp user create $MARIADB_USER $MARIADB_email --role=author --user_pass=$MARIADB_PWD --path=wordpress --allow-root
 
 fi
 chmod -R 0777 /var/www/
